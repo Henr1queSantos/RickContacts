821924a74c8c2e81f2043d5d45ee82385a889f25
Henr1queSantos <henrique.moraesps@gmail.com>
2021-06-03 11:59:42 -0300

    testando commit

--- RickNotifications/RickNotifications/Models/User.cs
+++ RickNotifications/RickNotifications/Models/User.cs
@@ -8,5 +8,7 @@ namespace RickNotifications.Models
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public int MyProperty { get; set; }
     }
 }
